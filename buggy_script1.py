01e4d80c79e932801dd1855654dcc14e32d0bfb5
brianthomaspark-oss <brian.thomas.park@gmail.com>
2025-09-14 16:59:42 -0400

    Add files via upload

---
+++ buggy_script1.py
@@ -0,0 +1,7 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # buggy_script_1.py (intentionally broken)
+... print "Hello"
+... prnt("Welcome to the program!")
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> 
